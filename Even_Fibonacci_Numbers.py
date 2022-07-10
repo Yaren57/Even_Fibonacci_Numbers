dd2bdd60b5d874a67d5c61ff550ad6f96b31bc5c
Şirin Yaren Değerli <76749230+Yaren57@users.noreply.github.com>
2022-07-10 15:33:08 +0300

    Update Even_Fibonacci_Numbers.py

--- Even_Fibonacci_Numbers.py
+++ Even_Fibonacci_Numbers.py
@@ -1,11 +1,9 @@
+a = 1
+b = 1
 toplam = 0
-sayi1 = 1
-sayi2= 2
-
-while (sayi2<4000000) :
-    sayi3 = sayi1 + sayi2
-    if (sayi3 % 2) == 0:
-        toplam += sayi3
-    sayi1 = sayi2
-    sayi2 = sayi3
-print(toplam)
+while a <= 4000000:
+    if a % 2 == 0:
+        toplam += a
+    a = b
+    b = a + b
+print (toplam)
